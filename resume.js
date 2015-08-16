aa7346c198482a4704ad890034bda3c6cbadc0b6
Kevin Jin <goldstar.kevin@gmail.com>
2015-08-16 16:48:42 -0400

    Improve reading order of exported resume PDFs Apply some hackery in HTML and CSS such that Chrome outputs a PDF with bullets points, dashes, and section headers in a logical order when the PDF is exported to a TXT. Hopefully enhances readability for Applicant Tracking Systems. Fix the layout compression of mobile resume printouts.

--- resume.js
+++ resume.js
@@ -1,3 +1,13 @@
+// Chrome's print to PDF is targeted because of PhantomJS
+function enhanceReadingOrder() {
+	// in print to PDF on Chrome, this ensures bullet marks are placed before
+	// the bullet text when bullet text spans multiple lines. you can test this
+	// by opening the generated PDF in Adobe Reader -> Save As -> Text (Accessible).
+	$('.entry li:not(.placeholder):not(.endline):not(.leftalign2col):not(.rightalign2col)').before($('<li style="display: inline"></li>'));
+	// TODO: figure out something that makes Chrome add blank lines to PDF text
+	//$('.section h2, .entry').before($('<pre>&nbsp;</pre><br />'));
+}
+
 $(document).ready(function() {
 	$.fn.extend({
 		bgLoaded: function (callback, timeout) {
@@ -26,4 +36,6 @@ $(document).ready(function() {
 			$sunglasses.transition('opacity 1s').css('opacity', '');
 		}, 2000);
 	}
+
+	enhanceReadingOrder();
 });
